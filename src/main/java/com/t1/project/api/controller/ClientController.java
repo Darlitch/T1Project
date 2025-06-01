bb24dce72b41bbd1a352c22c8a5c9ca459991cbf
Darlitch <errorguy@mail.ru>
2025-06-02 02:27:59 +0700

    All Controllers And dpcker-compose done

--- src/main/java/com/t1/project/api/controller/ClientController.java
+++ src/main/java/com/t1/project/api/controller/ClientController.java
@@ -1,4 +1,58 @@
 package com.t1.project.api.controller;
 
+import com.t1.project.api.dto.client.ClientCreateDto;
+import com.t1.project.api.dto.client.ClientDto;
+import com.t1.project.api.dto.client.ClientUpdateDto;
+import com.t1.project.core.service.ClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clients")
+@RequiredArgsConstructor
 public class ClientController {
+
+    private final ClientService clientService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ClientDto create(@RequestBody ClientCreateDto clientDto) {
+        return clientService.create(clientDto);
+    }
+
+    @GetMapping
+    public List<ClientDto> getAll () {
+        return clientService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ClientDto getById(@PathVariable Long id) {
+        return clientService.getById(id);
+    }
+
+    @GetMapping("/{lastName}")
+    public List<ClientDto> getByLastName(@PathVariable String lastName) {
+        return clientService.getByLastName(lastName);
+    }
+
+    @PutMapping("/{id}")
+    public ClientDto update(@PathVariable Long id, @RequestBody ClientUpdateDto clientDto) {
+        return clientService.update(id, clientDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        clientService.delete(id);
+    }
 }
