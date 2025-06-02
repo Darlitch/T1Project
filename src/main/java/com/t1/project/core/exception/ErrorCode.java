fc98085f8a825a88a4c5363ac86e5fe8d0af6062
Darlitch <errorguy@mail.ru>
2025-06-02 23:23:40 +0700

    ExceptionControllerAdvice  is done

---
+++ src/main/java/com/t1/project/core/exception/ErrorCode.java
@@ -0,0 +1,5 @@
+package com.t1.project.core.exception;
+
+public enum ErrorCode {
+    NOT_FOUND
+}
---
+++ src/main/java/com/t1/project/core/exception/ServiceException.java
@@ -0,0 +1,13 @@
+package com.t1.project.core.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ServiceException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public ServiceException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+}
